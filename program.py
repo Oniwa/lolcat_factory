d666609018f7dd7b34113859b07c13b83612a99d
John Barksdale <jdbarks@gmail.com>
2019-01-23 21:05:50 -0600

    Added print header function and get or create folder

--- program.py
+++ program.py
@@ -1,9 +1,34 @@
+import os
+
+
 def main():
-    # print the header
-    # get or create output folder
+    print_header()
+
+    folder = get_or_create_output_folder()
+    print(f'Found or created {folder}')
+
     # download cats
     # display cats
     print('Hello from main')
+
+
+def print_header():
+    print('------------------------------')
+    print('        LOLCat Factory')
+    print('------------------------------')
+    print()
+
+
+def get_or_create_output_folder():
+    base_folder = os.path.dirname(__file__)
+    folder = 'cat_pictures'
+    full_path = os.path.join(base_folder, folder)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print(f'Creating new directory at {full_path}')
+        os.mkdir(full_path)
+
+    return full_path
 
 
 if __name__ == '__main__':
