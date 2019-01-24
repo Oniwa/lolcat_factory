6679124b4a9f7ea7052d9fd15f46ec60f332143b
John Barksdale <jdbarks@gmail.com>
2019-01-23 20:56:00 -0600

    Initial commit

---
+++ program.py
@@ -0,0 +1,10 @@
+def main():
+    # print the header
+    # get or create output folder
+    # download cats
+    # display cats
+    print('Hello from main')
+
+
+if __name__ == '__main__':
+    main()
